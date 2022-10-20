6a5167d902c64c15ba81aa22fe4cdf700615fed9
FelipeSaCh <andres.sanchez346@pascualbravo.edu.co>
2022-10-19 23:04:49 -0500

    Proyecto

--- libreria/libreria/Class1.cs
+++ libreria/libreria/Class1.cs
@@ -76,7 +76,7 @@ namespace libreria
             {
                 return nic.GetPhysicalAddress().ToString();
             }
-            return "null";
+            return null;
         }
 
         public static string Patches()
